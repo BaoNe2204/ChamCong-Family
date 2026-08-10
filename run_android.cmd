4dd0c1e23ed8fd34cec1410dd9a5c0eabf5962d1
Nguyen Tran Gia Bao <minhtenbao24@gmail.com>
2026-08-11 00:22:59 +0700

    Add -c flag to clear expo cache to prevent auto-launching adb

--- run_android.cmd
+++ run_android.cmd
@@ -9,5 +9,5 @@ echo Dang cai dat thu vien neu chua co...
 call npm install
 echo.
 echo Khoi dong Expo (Che do Tunnel cho VPS)...
-call npx expo start --tunnel
+call npx expo start -c --tunnel
 pause
